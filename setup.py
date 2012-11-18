117ed2d3575842bbe8ab8b209a61fda09eaaf075
Seth M Morton <seth.m.morton@gmail.com>
2012-11-17 21:28:21 -0800

    Relying only on README.md, not on README, and got rid of ez_setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
-# In case setuptools is not installed
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # Read the _version.py file for the module version number
 import re
@@ -13,17 +13,16 @@ with open(VERSIONFILE, "rt") as fl:
     versionstring = fl.readline().strip()
 m = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionstring)
 if m:
-    version = m.group(1)
+    VERSION = m.group(1)
 else:
     s = "Unable to locate version string in {0}"
     raise RuntimeError (s.format(VERSIONFILE))
 
 setup(name='input_reader',
-      version=version,
+      version=VERSION,
       author='Seth M. Morton',
       author_email='drtuba78@gmail.com',
       url='https://github.com/SethMMorton/input_reader',
-      #download_url='',
       packages=['input_reader'],
       description='An easy interface for reading block- and keyword-type input files',
       #long_description='',
