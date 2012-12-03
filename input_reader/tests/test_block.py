43579ab1f49648a94caf199171a0f3d12941f0e4
Seth M Morton <seth.m.morton@gmail.com>
2012-12-03 00:01:39 -0800

    Added new tests for block keys and regex line

---
+++ input_reader/tests/test_block.py
@@ -0,0 +1,128 @@
+from input_reader import InputReader, ReaderError, SUPPRESS, Namespace
+from pytest import raises, fixture
+from re import search
+
+def test_block_missing_keyname():
+    r = InputReader()
+    with raises(TypeError):
+        r.add_block_key()
+    with raises(TypeError):
+        r.add_block_key(end='subend')
+    with raises(TypeError):
+        r.add_block_key(ignoreunknown=True)
+    with raises(TypeError):
+        r.add_block_key(case=False)
+
+def test_block_correct_call():
+    r = InputReader()
+    a = r.add_block_key('red')
+    assert a.name == 'red'
+    assert a._end == 'end'
+    assert not a._case
+    assert not a._ignoreunknown
+
+def test_block_name_definition():
+    r = InputReader()
+    with raises(ValueError) as e:
+        r.add_block_key(23)
+    assert 'keyname must be str' in str(e.value)
+
+def test_block_repeat_in_definition():
+    # You cannot repeat keys
+    r = InputReader()
+    r.add_block_key('red')
+    with raises(ReaderError) as e:
+        r.add_block_key('red')
+    assert search(r'The key \w+ has been defined twice', str(e.value))
+
+def test_block_end_definition():
+    r = InputReader()
+    a = r.add_block_key('red', end='subend')
+    assert a._end == 'subend'
+    with raises(ValueError) as e:
+        r.add_block_key('blue', end=23)
+    assert 'end must be str' in  str(e.value)
+    
+def test_block_case_definition():
+    r = InputReader()
+    a = r.add_block_key('red', case=True)
+    assert a._case
+    with raises(ValueError) as e:
+        r.add_block_key('blue', case='True')
+    assert 'case must be bool' in str(e.value)
+
+def test_block_ignoreunknown_definition():
+    r = InputReader()
+    a = r.add_block_key('red', ignoreunknown=True)
+    assert a._ignoreunknown
+    with raises(ValueError) as e:
+        b = r.add_block_key('blue', ignoreunknown='True')
+    assert 'ignoreunknown must be bool' in str(e.value)
+
+def test_block_read_using_defaults():
+    r = InputReader()
+    a = r.add_block_key('blue')
+    inp = r.read_input(['blue', 'end'])
+    assert inp.blue == Namespace()
+    b = r.add_block_key('red')
+    b.add_boolean_key('rose', default=False)
+    b.add_boolean_key('rider', default=False)
+    inp = r.read_input(['red', 'rose', 'end'])
+    assert inp.red.rose
+    assert not inp.red.rider
+    inp = r.read_input(['red', 'end'])
+    assert not inp.red.rose
+    assert not inp.red.rider
+    c = r.add_block_key('cyan')
+    with raises(ReaderError) as e:
+        inp = r.read_input(['cyan word', 'end'])
+    assert search(r'The block "\w+" was given arguments, this is illegal',
+                  str(e.value))
+
+def test_block_read_end():
+    r = InputReader()
+    a = r.add_block_key('red', end='subend')
+    a.add_boolean_key('rose', default=False)
+    inp = r.read_input(['red', 'rose', 'subend'])
+    assert inp.red.rose
+    inp = r.read_input(['red', 'subend'])
+    assert not inp.red.rose
+
+def test_block_read_case_sensitive():
+    r = InputReader()
+    a = r.add_block_key('red', case=True, end='END')
+    a.add_boolean_key('ROSE')
+    inp = r.read_input(['RED', 'ROSE', 'END'])
+    assert not hasattr(inp.red, 'rose')
+    assert inp.red.ROSE
+    b = r.add_block_key('pink', case=False)
+    b.add_boolean_key('ROSE')
+    inp = r.read_input(['PINK', 'ROSE', 'END'])
+    assert not hasattr(inp.pink, 'ROSE')
+    assert inp.pink.rose
+    
+def test_block_read_ignoreunknown():
+    r = InputReader()
+    a = r.add_block_key('red', ignoreunknown=False)
+    a.add_boolean_key('rose')
+    with raises(ReaderError) as e:
+        inp = r.read_input(['red', 'rose', 'rider', 'end'])
+    assert 'Unrecognized key' in str(e.value)
+    b = r.add_block_key('blue', ignoreunknown=True)
+    b.add_boolean_key('rose')
+    inp = r.read_input(['blue', 'rose', 'rider', 'end'])
+    assert inp.blue.rose
+    assert not hasattr(inp.blue, 'rider')
+
+def test_block_read_subblocks():
+    r = InputReader()
+    a = r.add_block_key('red')
+    b = a.add_block_key('blue')
+    b.add_boolean_key('egg')
+    inp = r.read_input(['red', 'blue', 'egg', 'end', 'end'])
+    assert inp.red.blue.egg
+    c = r.add_block_key('pink')
+    d = c.add_block_key('blue', end='subend')
+    d.add_boolean_key('egg')
+    inp = r.read_input(['pink', 'blue', 'egg', 'subend', 'end'])
+    assert inp.pink.blue.egg
---
+++ input_reader/tests/test_regex.py
@@ -0,0 +1,76 @@
+from input_reader import InputReader, ReaderError, SUPPRESS
+from pytest import raises, fixture
+import re
+from re import search
+
+def test_regex_missing_handle():
+    r = InputReader()
+    with raises(TypeError):
+        r.add_regex_line()
+    with raises(TypeError):
+        r.add_regex_line('red')
+
+def test_regex_correct_call():
+    r = InputReader()
+    a = r.add_regex_line('red', r'funny\d+dog')
+    assert a.name == 'red'
+    assert a._regex.pattern == r'funny\d+dog'
+    assert not a._case
+    regex = re.compile(r'funny\d+dog')
+    b = r.add_regex_line('blue', regex)
+    assert b.name == 'blue'
+    assert b._regex == regex
+    assert not b._case
+
+def test_regex_case_definition():
+    r = InputReader()
+    a = r.add_regex_line('red', r'funny\d+DOG')
+    assert a.name == 'red'
+    assert a._regex.pattern == r'funny\d+DOG'
+    assert a._regex.flags == re.IGNORECASE
+    regex = re.compile(r'funny\d+dog', re.IGNORECASE)
+    b = r.add_regex_line('blue', regex)
+    assert b.name == 'blue'
+    assert b._regex == regex
+    c = r.add_regex_line('green', r'funny\d+DOG', case=True)
+    assert c.name == 'green'
+    assert c._regex.pattern == r'funny\d+DOG'
+    assert c._regex.flags != re.IGNORECASE
+    regex = re.compile(r'funny\d+dog', re.IGNORECASE)
+    # Case is ignored if a regex flag is given
+    d = r.add_regex_line('gray', regex, case=True)
+    assert d.name == 'gray'
+    assert d._regex == regex
+
+def test_regex_handle_definition():
+    r = InputReader()
+    with raises(ValueError) as e:
+        r.add_regex_line(23, r'test')
+    assert 'handle must be str' in str(e.value)
+
+def test_regex_repeat_in_definition():
+    # You cannot repeat keys
+    r = InputReader()
+    r.add_regex_line('red', r'test')
+    with raises(ReaderError) as e:
+        r.add_regex_line('red', r'tax')
+    regex = r'The key \w+ has been defined twice'
+    assert search(regex, str(e.value))
+
+def test_regex_read():
+    r = InputReader()
+    # Remember it's case-insensitive
+    r.add_regex_line('red', r'funny(\d+)DOG\s*(kitty)?')
+    inp = r.read_input(['funny14dog'])
+    assert inp.red.group(0) == 'funny14dog'
+    assert inp.red.group(1) == '14'
+    inp = r.read_input(['funny12DOG kitty'])
+    # Regex won't lowercase for you
+    assert inp.red.group(0) == 'funny12DOG kitty'
+    assert inp.red.group(1) == '12'
+    assert inp.red.group(2) == 'kitty'
+    r.add_regex_line('blue', r'(SILLY|ODD)\s*(goose|duck)', case=True)
+    inp = r.read_input(['SILLY goose'])
+    assert inp.blue.group(0) == 'SILLY goose'
+    inp = r.read_input(['ODD duck'])
+    assert inp.blue.group(0) == 'ODD duck'
