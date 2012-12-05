1abb0fe02dbe38cf4f40c7f98df63890d357263e
Seth M Morton <seth.m.morton@gmail.com>
2012-12-04 22:59:31 -0800

    Added documentation to Namespace

--- input_reader/helpers.py
+++ input_reader/helpers.py
@@ -2,13 +2,19 @@ from __future__ import division, print_function
 
 class Namespace(object):
     '''A simple class to hold the keys and aruments found from the
-    input file.  This is a copy-and-paste job from the argparse module.
+    input file.  This is a copy-and-paste job from the argparse module
+    with some minor additions.
+
+    You can populate the Namespace when initiallization with a series
+    of key-value pairs.
     '''
 
     def __init__(self, **kwargs):
+        self._order = []
         for name in kwargs:
             setattr(self, name, kwargs[name])
-        self._order = []
+            if name not in self._order:
+                self._order.append(name)
 
     def __repr__(self):
         type_name = type(self).__name__
@@ -19,7 +25,7 @@ class Namespace(object):
 
     def _get_kwargs(self):
         try:
-            return [(k, self.__dict__[k]) for k in self._order]
+            return [(k, getattr(self, k)) for k in self._order]
         except AttributeError:
             return sorted(self.__dict__.items())
 
@@ -43,6 +49,16 @@ class Namespace(object):
         return len(self._order)
 
     def add(self, key, val):
+        '''\
+        Add a key-value pair to the :py:class:`Namespace`.
+
+        :argument key:
+            The key to add
+        :type key: :py:class:`str`
+        :argument val:
+            The value of the key
+        :type val: :py:class:`any`
+        '''
         setattr(self, key, val)
         # Add this to the list of things found in the order found.
         # First check that it doesn't exist because duplicates can be
@@ -51,6 +67,13 @@ class Namespace(object):
             self._order.append(key)
 
     def remove(self, key):
+        '''\
+        Remove a key from the :py:class:`Namespace`.
+
+        :argument key:
+            The key to remove.  If it does not exist, it is ignored.
+        :type key: :py:class:`str`
+        '''
         try:
             delattr(self, key)
         except AttributeError:
@@ -61,22 +84,48 @@ class Namespace(object):
             pass
 
     def get(self, key, default=None):
-        try:
-            return self.__dict__[key]
-        except KeyError:
-            return default
+        '''\
+        Get the value of a key.  If the key does not exist, 
+        `default` is returned. This is alternative to the
+        namespace.key syntax that does not raise an error
+        when `key` does not exist.
+
+        :argument key: The key whose value you wish to retrieve.
+        :type key: :py:const:`str`
+        :argument default:
+        :type default: :py:class:`any` (default is :py:const:`None`)
+        :returns: The value associated with `key`
+        '''
+        return getattr(self, key, default)
 
     def keys(self):
+        '''\
+        Just like the :py:meth:`keys` function for the python 
+        :py:class:`dict`.
+
+        :returns: :py:class:`tuple` of all keys in the :py:class:`Namespace`.
+        '''
         return tuple(self._order)
 
     def values(self):
-        return tuple([self.__dict__[x] for x in self._order])
+        '''\
+        Just like the :py:meth:`values` function for the python 
+        :py:class:`dict`.
+
+        :returns: :py:class:`tuple` of all values in the 
+                  :py:class:`Namespace`.
+        '''
+        return tuple([getattr(self, x) for x in self._order])
 
     def items(self):
-        return tuple([(x, self.__dict__[x]) for x in self._order])
+        '''\
+        Just like the :py:meth:`items` function for the python 
+        :py:class:`dict`.
 
-    def make_set(self):
-        return set(self.__dict__.keys()) - set(['_order'])
+        :returns: :py:class:`tuple` of :py:class:`tuple`s of 
+                  all key, value pairs in the :py:class:`Namespace`.
+        '''
+        return tuple([(x, getattr(self, x)) for x in self._order])
 
 class ReaderError(Exception):
     '''\
