be7d3124aec73bf16eea6e5a8d06821a463107ae
Seth M Morton <seth.m.morton@gmail.com>
2012-12-15 01:59:11 -0800

    Added ability to run doctests from sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Doctest path
+doctest_path = [os.path.abspath(os.path.join(os.curdir, '..', '..'))]
+
 # The suffix of source filenames.
 source_suffix = '.rst'
 
